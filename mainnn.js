da3a94897a3d9ca47dd6528b3625d818d137c028
melikedemir2003 <134722658+melikedemir2003@users.noreply.github.com>
2023-06-29 12:59:49 +0300

    Update mainnn.js

--- mainnn.js
+++ mainnn.js
@@ -1,1 +1,32 @@
-
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+document.querySelector('form').addEventListener('submit', function(event) {
+event.preventDefault(); // Sayfanın yeniden yüklenmesini engellemek için
+    
+var password = document.getElementById('password').value;
+var repeatPassword = document.getElementById('repeatpassword').value;
+    
+var errorMessage = '';
+if (password.length < 8) {
+errorMessage += 'Parola en az 8 karakter olmalıdır.<br>';
+}
+if (password !== repeatPassword) {
+errorMessage += 'Parolalar eşleşmiyor.';
+}
+if (errorMessage !== '') {
+showError(errorMessage);
+} else {
+// Formu gönder
+this.submit();
+}
+});
+function showError(message) {
+var errorDiv = document.createElement('div');
+errorDiv.className = 'error-message';
+errorDiv.innerHTML = message;
+    
+var form = document.querySelector('form');
+form.appendChild(errorDiv);
+}
+});
+</script>
